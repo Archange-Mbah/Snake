176b659195d937ed9284483066dd990913c40f17
Archange Mbah Tchategouang <archange.mbah.tchategouang@mni.thm.de>
2024-01-13 15:08:49 +0100

    Snake

--- src/Model/SnakeThread.java
+++ src/Model/SnakeThread.java
@@ -17,7 +17,7 @@ public class SnakeThread  extends Thread{
         while(true){
             try{
                 Thread.sleep(1000);
-                model.getGoodFood().setX(random.nextInt(4,25));
+                model.getGoodFood().setX(random.nextInt(3,25));
                 model.getGoodFood().setY(random.nextInt(3,20));
                 model.getBadFood().setX(random.nextInt(5,25));
                 model.getBadFood().setY(random.nextInt(4,20));
