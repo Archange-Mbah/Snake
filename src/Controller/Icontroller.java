19ab2050da17f77e20e440aa7e694d7769f3bfaf
Archange Mbah Tchategouang <archange.mbah.tchategouang@mni.thm.de>
2024-01-13 16:46:08 +0100

    Modified

--- src/Controller/Icontroller.java
+++
@@ -1,7 +0,0 @@
-package Controller;
-
-public interface Icontroller {
-
-    void nextFrame();
-    void userInput(int a);
-}
--- src/Model/Direction.java
+++
@@ -1,8 +0,0 @@
-package Model;
-
-/**
- * Direction is an enum class that contains the different directions of the snake
- */
-public enum Direction {
-     RIGHT, LEFT, UP, DOWN
-}
